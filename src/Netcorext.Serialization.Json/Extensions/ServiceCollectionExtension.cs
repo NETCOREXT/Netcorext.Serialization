0c300410afc58f3a7f6d0c69e9656eb5945bc71e
Aren Chen <gucci313031@gmail.com>
2022-12-12 14:22:16 +0800

    add TryAdd

--- src/Netcorext.Serialization.Json/Extensions/ServiceCollectionExtension.cs
+++ src/Netcorext.Serialization.Json/Extensions/ServiceCollectionExtension.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using MessagePack;
 using MessagePack.Resolvers;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
 using Netcorext.Serialization;
 using Netcorext.Serialization.Json;
@@ -17,6 +18,12 @@ public static class ServiceCollectionExtension
         services.AddSingleton<ISerializer, TSerializer>();
 
         return services;
+    }
+
+    public static void TryAddJsonSerializer<TSerializer>(this IServiceCollection services)
+        where TSerializer : class, ISerializer
+    {
+        services.TryAddSingleton<ISerializer, TSerializer>();
     }
 
     public static IServiceCollection AddSystemJsonSerializer(this IServiceCollection services, Action<IServiceProvider, JsonSerializerOptions>? configure = null)
@@ -44,6 +51,29 @@ public static class ServiceCollectionExtension
         return services;
     }
 
+    public static void TryAddSystemJsonSerializer(this IServiceCollection services, Action<IServiceProvider, JsonSerializerOptions>? configure = null)
+    {
+        services.TryAddSingleton<ISerializer>(provider =>
+                                              {
+                                                  var options = new JsonSerializerOptions
+                                                                {
+                                                                    PropertyNameCaseInsensitive = true,
+                                                                    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+                                                                    WriteIndented = false,
+                                                                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                                                                    ReferenceHandler = ReferenceHandler.IgnoreCycles,
+                                                                    NumberHandling = JsonNumberHandling.AllowReadingFromString
+                                                                };
+
+                                                  configure?.Invoke(provider, options);
+
+                                                  var loggerFactory = provider.GetRequiredService<ILoggerFactory>();
+                                                  var logger = loggerFactory.CreateLogger<SystemJsonSerializer>();
+
+                                                  return new SystemJsonSerializer(options, logger);
+                                              });
+    }
+
     public static IServiceCollection AddMsgPackJsonSerializer(this IServiceCollection services, Action<IServiceProvider, MessagePackSerializerOptions>? configure = null)
     {
         services.AddSingleton<ISerializer, MsgPackJsonSerializer>(provider =>
@@ -68,5 +98,29 @@ public static class ServiceCollectionExtension
                                                                   });
 
         return services;
+    }
+
+    public static void TryAddMsgPackJsonSerializer(this IServiceCollection services, Action<IServiceProvider, MessagePackSerializerOptions>? configure = null)
+    {
+        services.TryAddSingleton<ISerializer>(provider =>
+                                              {
+                                                  var dateTimeResolver = CompositeResolver.Create(new DateTimeFormatter(),
+                                                                                                  new NullableDateTimeFormatter(),
+                                                                                                  new DateTimeOffsetFormatter(),
+                                                                                                  new NullableDateTimeOffsetFormatter());
+
+                                                  var resolver = CompositeResolver.Create(dateTimeResolver, TypelessContractlessStandardResolver.Instance);
+
+                                                  var options = TypelessContractlessStandardResolver.Options
+                                                                                                    .WithResolver(resolver)
+                                                                                                    .WithCompression(MessagePackCompression.Lz4BlockArray);
+
+                                                  configure?.Invoke(provider, options);
+
+                                                  var loggerFactory = provider.GetRequiredService<ILoggerFactory>();
+                                                  var logger = loggerFactory.CreateLogger<MsgPackJsonSerializer>();
+
+                                                  return new MsgPackJsonSerializer(options, logger);
+                                              });
     }
 }
